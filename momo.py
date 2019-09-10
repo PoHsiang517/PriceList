4de68b05b242b8e8725f358c49721916dd82ce63
HsiangTW <huang.pohsiang@gmail.com>
2019-09-10 17:53:18 +0800

    update

--- momo.py
+++ momo.py
@@ -8,7 +8,7 @@ ua = UserAgent()
 #url = "https://m.momoshop.com.tw/search.momo?searchKeyword=43pfh4052&couponSeq=&searchType=1&cateLevel=-1&ent=k&_imgSH=fourCardStyle"
 url = "https://m.momoshop.com.tw/search.momo?searchKeyword=%E8%97%8D%E7%89%99%E8%80%B3%E6%A9%9F&couponSeq=&searchType=1&cateLevel=-1&ent=k&_imgSH=fourCardStyle"
 headers = {'User-Agent':ua.chrome}
-r = requests.get(url, headers = headers)
+r = requests.get(url, headers=headers)
 
 #print(r.status_code)
 #print(r.history)
@@ -23,6 +23,6 @@ discount = soup.find_all("p", class_="prdEvent") #撈不到 /滿n件現折xxx/
 prices = soup.find_all("b", class_="price")
 
 for c, i, p in zip(contents, items, prices):
-	print(i.contents[0].strip())
-	print(p.contents[0])
+    print(i.contents[0].strip())
+    print(p.contents[0])
 
