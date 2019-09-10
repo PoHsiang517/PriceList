66e2ff8f8a86b211abf45a491c367ed3e8439c69
HsiangTW <huang.pohsiang@gmail.com>
2019-09-10 15:32:41 +0800

    momo try mobile.ver

--- momo.py
+++ momo.py
@@ -1,12 +1,26 @@
 import requests
+import json
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
-url = "https://www.momoshop.com.tw/search/searchShop.jsp?keyword=43pfh4052&searchType=1&curPage=1&_isFuzzy=0&showType=chessboardType"
-headers = {'User-Agent': 'Googlebot', 'From': 'EMAIL'}
 
-r = requests.get(url, headers = headers, allow_redirects = False)
+ua = UserAgent()
+改用行動版網頁即可抓到MOMO搜尋出的商品
+url = "https://m.momoshop.com.tw/search.momo?searchKeyword=43pfh4052&couponSeq=&searchType=1&cateLevel=-1&ent=k&_imgSH=fourCardStyle"
+headers = {'User-Agent':ua.chrome}
+r = requests.get(url, headers = headers)
 
 #print(r.status_code)
 #print(r.history)
 #print(r.url)
 
-#soup = BeautifulSoup(r.text, 'html.parser')
+soup = BeautifulSoup(r.text, 'html.parser')
+
+contents = soup.find_all("article", class_="prdListArea")
+items = soup.find_all("p", class_="prdName")
+prices = soup.find_all("b", class_="price")
+
+#print(prices)
+
+for c, i, p in zip(contents, items, prices):
+	print(i.contents[0].strip())
+	print(p.contents[0])
